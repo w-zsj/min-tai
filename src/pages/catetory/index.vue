9bddc8f70d76512843fff20af3511869df4686e4
ht17601336017 <836874519@qq.com>
2022-07-29 11:21:38 +0800

    购物车页面

--- src/pages/catetory/index.vue
+++ src/pages/catetory/index.vue
@@ -6,19 +6,35 @@
         <view> AAAA </view>
       </scroll-view>
       <scroll-view class="cates_right">
-        <view class="banner">
-          <image src="/static/images/food.jpg">
-        </view>
-        <view class="list">
-          <view class="title">
-            <text class="line">/</text>
-            <text decode>&emsp;张三&emsp;</text>
-            <text class="line">/</text>
+        <view class="cates_right_area">
+          <view class="banner">
+            <image src="/static/images/food.jpg" />
           </view>
-          <view class="content">
-            <view>
-              <image src="/static/images/food.jpg" />
-              <text>张三</text>
+          <view class="list">
+            <view class="title">
+              <text decode>语农&emsp;散装土鸡蛋&emsp;260枚&emsp;40斤</text>
+              <text decode>提货时间:&emsp;08月13日</text>
+              <view class="cate-info">
+                <text class="cate-price">¥ 57.6</text>
+                <text class="cate-sale">已售2700件</text>
+                <view class="add-cate">加入购物车</view>
+              </view>
+            </view>
+          </view>
+        </view>
+        <view class="cates_right_area">
+          <view class="banner">
+            <image src="/static/images/food.jpg" />
+          </view>
+          <view class="list">
+            <view class="title">
+              <text decode>语农&emsp;散装土鸡蛋&emsp;260枚&emsp;40斤</text>
+              <text decode>提货时间:&emsp;08月13日</text>
+              <view class="cate-info">
+                <text class="cate-price">¥ 57.6</text>
+                <text class="cate-sale">已售2700件</text>
+                <view class="add-cate">加入购物车</view>
+              </view>
             </view>
           </view>
         </view>

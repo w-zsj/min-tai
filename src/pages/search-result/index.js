9831d06d75905245029bc13adf88aaacf79c6fe2
zhangsanjun <zhangsanjun@eyee.com>
2022-08-02 15:21:48 +0800

    搜索结果页

--- src/pages/search-result/index.js
+++ src/pages/search-result/index.js
@@ -1,7 +1,42 @@
 import serachInput from "@comps/serach-input/index.vue";
+import productUnit from "@/components/product-unit/cell.vue";
+import skuModal from "@/components/sku-modal/index.vue";
+import {debounce} from '@/utils/util'
+const app = getApp();
+let _;
 export default {
-    components: { serachInput },
+    components: { serachInput, productUnit, skuModal },
     data() {
-        return{}
-    }
+        _ = this;
+        return {
+            prodList: [],
+            productId:'',
+            isShowAuthPhone: false,
+            selectSkuModalShow:false
+        }
+    },
+    methods: {
+        customevent: debounce(
+            function (params) {
+              if (app.hasmobile()) {
+                Object.assign(_, {
+                  selectSkuModalShow: true,
+                  productId: params.item.id,
+                });
+              } else _.isShowAuthPhone = true;
+            },
+            500,
+            true
+          ),
+          checkHasMobile(cb) {
+            if (app.globalData.hasmobile()) {
+              cb();
+            } else {
+              _.isShowAuthPhone = true;
+            }
+        },
+        changeValue(val) {
+            console.log('val',val)
+        }
+    },
 }
--- src/pages/search-result/index.vue
+++ src/pages/search-result/index.vue
@@ -2,6 +2,33 @@
   <view class="search-result">
     <!-- 搜索 -->
     <serachInput :changeValue="changeValue" :source="2"></serachInput>
+    <view class="prd-list">
+      <block v-for="(item, index) in prodList" :key="index">
+        <view class="skeleton-rect">
+          <product-unit
+            className="catetory"
+            :index="index"
+            :itemClone="item"
+            @customevent="customevent"
+          >
+          </product-unit>
+        </view>
+      </block>
+    </view>
+
+    <!-- 加入购物车 -->
+    <sku-modal
+      v-if="selectSkuModalShow"
+      :productId="item.id"
+      source="catetory"
+      @close="selectSkuModalShow = false"
+    >
+    </sku-modal>
+    <!-- 授权手机号 -->
+    <authority-phone-modal
+      @closemodal="isShowAuthPhone=false"
+      :isShowAuthPhone="isShowAuthPhone"
+    ></authority-phone-modal>
   </view>
 </template>
 <script src="./index.js"></script>
